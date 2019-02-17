63257909eb510d760ace2b7f41b07113d233a74e
Grant Horner <h.grant.horner@gmail.com>
2019-02-17 12:14:31 -0500

    seeded db

--- db/index.js
+++ db/index.js
@@ -1,7 +1,11 @@
 const cnxn = require('./cnxn')
 const models = require('./models')
+const syncAndSeed = require('./syncAndSeed')
+
+syncAndSeed()
 
 module.exports = {
   cnxn,
-  models
+  models,
+  syncAndSeed
 }
--- db/syncAndSeed.js
+++ db/syncAndSeed.js
@@ -1,1 +1,28 @@
-const { cnxn, models } = require('./')
+const cnxn = require('./cnxn')
+const { Page, Content } = require('./models')
+
+Page.hasMany(Content)
+Content.belongsTo(Page)
+
+const syncAndSeed = () => {
+  return cnxn.sync({ force: true }).then(async () => {
+    const [home, employees, contact] = await Promise.all([
+      Page.create({ title: 'Home' }),
+      Page.create({ title: 'Employees' }),
+      Page.create({ title: 'Contact ' })
+    ])
+
+    await Promise.all([
+      Content.create({ name: 'Welcome Home 1', body: 'xoxoxo', pageId: home.id }),
+      Content.create({ name: 'Welcome Home 2', body: 'xoxoxo', pageId: home.id }),
+      Content.create({ name: 'LARRY', body: 'CTO', pageId: employees.id }),
+      Content.create({ name: 'CURLY', body: 'COO', pageId: employees.id }),
+      Content.create({ name: 'MOE', body: 'CEO', pageId: employees.id }),
+      Content.create({ name: 'Phone', body: '212-555-1212', pageId: contact.id }),
+      Content.create({ name: 'Telex', body: '212-555-1213', pageId: contact.id }),
+      Content.create({ name: 'FAX', body: '212-555-1214', pageId: contact.id })
+    ])
+  })
+}
+
+module.exports = syncAndSeed
